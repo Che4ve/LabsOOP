74b4ee218aff135144a962428eb844205556567f
Che4ve <chesnokov_vd@mail.ru>
2023-09-22 12:27:53 +0300

    haha

--- 1/test/tests.cpp
+++ 1/test/tests.cpp
@@ -2,11 +2,27 @@
 #include "../include/timecalc.h"
 
 TEST(test_01, basic_test_get) {
-    ASSERT_TRUE(convertTo24HourFormat(12, 22, "am") == "1222");
+    ASSERT_TRUE(convertTo24HourFormat(0, 0, "am") == "0000");
 }
 
 TEST(test_02, basic_test_get) {
-    ASSERT_TRUE(convertTo24HourFormat(0, 30, "am") == "0030");
+    ASSERT_TRUE(convertTo24HourFormat(0, 0, "pm") == "1200");
+}
+
+TEST(test_03, basic_test_get) {
+    ASSERT_TRUE(convertTo24HourFormat(0, 3, "am") == "0003");
+}
+
+TEST(test_04, basic_test_get) {
+    ASSERT_TRUE(convertTo24HourFormat(4, 30, "am") == "0430");
+}
+
+TEST(test_05, basic_test_get) {
+    ASSERT_TRUE(convertTo24HourFormat(0, 30, "pm") == "1230");
+}
+
+TEST(test_06, basic_test_get) {
+       ASSERT_TRUE(convertTo24HourFormat(11, 30, "pm") == "2330");
 }
 
 int main(int argc, char **argv){
