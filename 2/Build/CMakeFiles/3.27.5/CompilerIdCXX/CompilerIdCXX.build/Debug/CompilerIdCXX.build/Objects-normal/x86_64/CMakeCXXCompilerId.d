bb4c34d1391ca57cc705c5ae96969fd7f2994816
Che4ve <127440818+Che4ve@users.noreply.github.com>
2023-10-01 22:18:48 +0300

    Delete 2/Build directory

--- 2/Build/CMakeFiles/3.27.5/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/che4ve/MAI/LabsOOP/2/Build/CMakeFiles/3.27.5/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- 2/Build/build/lab_lib.build/Debug/Objects-normal/x86_64/thirteen.d
+++
@@ -1,3 +0,0 @@
-dependencies: /Users/che4ve/MAI/LabsOOP/2/src/thirteen.cpp \
-  /Users/che4ve/MAI/LabsOOP/2/src/../include/thirteen.h \
-  /Users/che4ve/MAI/LabsOOP/2/include/./vector13.h
