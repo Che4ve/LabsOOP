b44b536692320574ae78514cff157fc5e50c620a
Che4ve <127440818+Che4ve@users.noreply.github.com>
2023-10-01 22:18:36 +0300

    Delete 1/Build directory

--- 1/Build/CMakeFiles/timeconverter_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libtimeconverter_lib.a"
-)
--- 1/Build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/che4ve/MAI/LabsOOP/1
-# Build directory: /Users/che4ve/MAI/LabsOOP/1/Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[BASIC]=] "/Users/che4ve/MAI/LabsOOP/1/Build/tests")
-set_tests_properties([=[BASIC]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/che4ve/MAI/LabsOOP/1/CMakeLists.txt;39;add_test;/Users/che4ve/MAI/LabsOOP/1/CMakeLists.txt;0;")
